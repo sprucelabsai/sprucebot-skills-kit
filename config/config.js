92a7dcaa3d0187903ad3baf105b06b2b5afd4053
artisgarbage <artisgarbage@gmail.com>
2017-10-20 17:50:28 -0600

    more DX around .env setup

--- config/config.js
+++ config/config.js
@@ -2,7 +2,9 @@ const Config = {
 	API_KEY: process.env.API_KEY
 		? process.env.API_KEY
 		: 'you should have a .env file with API_KEY filled in for your skill',
-	HOST: process.env.HOST ? process.env.HOST : 'https://your.dev.sprucebot.com',
+	HOST: process.env.HOST
+		? process.env.HOST
+		: 'you should have a .env file with HOST filled in for your skill',
 	SKILL_ID: process.env.SKILL_ID
 		? process.env.SKILL_ID
 		: 'you should have a .env file with SKILL_ID filled in for your skill',
--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require('dotenv')
+require('dotenv').config()
 const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
@@ -11,9 +11,20 @@ const handle = app.getRequestHandler()
 
 class Sprucebot {
 	constructor(configObj) {
+		// Check against defaults to see if dev has updated their env vars
+		if (
+			configObj.apiKey.indexOf(' ') >= 0 ||
+			configObj.skillId.indexOf(' ') >= 0 ||
+			configObj.host.indexOf(' ') >= 0
+		) {
+			throw new Error(
+				'Oi! You need to create an .env file at the root of this repository! 😠  Jk, but for real, you should take a peak at .env.sample and then customize your own but just call it .env . Alternatively, if you prefer, you can use an awesome tool like PM2, Nodemon, or ForeverJS to manage this server-process and environment.'
+			)
+		}
+
+		// Check required configs for instantiating the class
 		const configParamErrorBaseStr =
 			'Sprucebot Class requires a single configuration object including'
-		// Check required configs for class
 		if (!configObj.apiKey) {
 			throw new Error(`${configParamErrorBaseStr} an apiKey key/value pair`)
 		} else if (!configObj.host) {
@@ -25,8 +36,8 @@ class Sprucebot {
 		this.host = configObj.host
 		this.skillId = configObj.skillId
 		console.log(
-			`Sprucebot Instantiated with \napiKey : ${this.apiKey}, \nhost : ${this
-				.host}, \nskillId : ${this.skillId}`
+			`Sprucebot Instantiated with : \napiKey : ${this.apiKey}, \nhost : ${this
+				.host}, \nskillId : ${this.skillId} \n---------------------------------`
 		)
 	}
 }
