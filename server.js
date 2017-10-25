ffb166dff3843cc9a56aa6ae40b493d96afb3f58
artisgarbage <artisgarbage@gmail.com>
2017-10-24 23:19:24 -0600

    added Koa and updated server to use Koa

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 require('dotenv').config()
-const { createServer } = require('http')
-const { parse } = require('url')
+
+const Koa = require('koa')
 const next = require('next')
+const Router = require('koa-router')
+
 const Config = require('./config/config')
 
-const port = parseInt(Config.PORT, 10) || 3333
-const dev = Config.NODE_ENV !== 'production'
+const port = parseInt(process.env.PORT, 10) || 3000
+const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
@@ -58,14 +60,37 @@ const sprucebot = new Sprucebot({
 	skillName: Config.SKILL_NAME
 })
 
-app.prepare().then(() => {
-	createServer((req, res) => {
-		const parsedUrl = parse(req.url, true)
 
-		// Render the route using next.js route handling
-		handle(req, res, parsedUrl)
-	}).listen(port, err => {
-		if (err) throw err
-		console.log(`🌲 Sprucebot Skill 🌲 ready on http://localhost:${port}`)
+app.prepare()
+	.then(() => {
+		const server = new Koa()
+		const router = new Router()
+
+		// BEGIN {custom-routes}
+		/*router.get('/a', async ctx => {
+			await app.render(ctx.req, ctx.res, '/b', ctx.query)
+			ctx.respond = false
+		})
+
+		router.get('/b', async ctx => {
+			await app.render(ctx.req, ctx.res, '/a', ctx.query)
+			ctx.respond = false
+		})*/
+		// END {custom-routes}
+
+		router.get('*', async ctx => {
+			await handle(ctx.req, ctx.res)
+			ctx.respond = false
+		})
+
+		server.use(async (ctx, next) => {
+			ctx.res.statusCode = 200
+			await next()
+		})
+
+		server.use(router.routes())
+		server.listen(port, (err) => {
+			if (err) throw err
+			console.log(`> Ready on http://localhost:${port}`)
+		})
 	})
-})
