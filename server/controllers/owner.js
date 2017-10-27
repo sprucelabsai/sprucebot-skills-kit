69c97fab4a84240c3f2077e488816eecc94e4d3e
Shane McDermed <shmcdermed@gmail.com>
2017-10-27 14:28:51 -0600

    Fix lint on owner controller...

--- server/controllers/owner.js
+++ server/controllers/owner.js
@@ -1,12 +1,11 @@
-module.exports = {
-
+module.exports = router => {
 	router.get('saveLocation', '/location/:locationId/save', async ctx => {
- 		const newCode = ctx.sb.utilites.rewards.generateDiscountCode()
+		const newCode = ctx.sb.utilites.rewards.generateDiscountCode()
 		const rewards = await ctx.sb.services.rewards.generateRewardWithCode(
 			ctx.req.params.locationId,
 			newCode
 		)
-	});
+	})
 
 	// assuming appointments are stored in a local postrges/mysql/sqllite database
 	router.get('getAppointments', '/location/:locationId/save', async ctx => {
@@ -14,14 +13,17 @@ module.exports = {
 			locationId: ctx.req.params.locationId
 		})
 		return ctx.res.json(appointments.map(a => a.toPublic()))
-	});
+	})
 
 	// post only
-	router.post('saveRewards', '/location/:locationId/rewards/save', async ctx => {
-		const appointments = ctx.sb.models.appointment.find({
-			locationId: req.params.locationId
-		})
-		return res.json(appointments.map(a => a.toPublic()))
-	});
-
+	router.post(
+		'saveRewards',
+		'/location/:locationId/rewards/save',
+		async ctx => {
+			const appointments = ctx.sb.models.appointment.find({
+				locationId: req.params.locationId
+			})
+			return res.json(appointments.map(a => a.toPublic()))
+		}
+	)
 }
