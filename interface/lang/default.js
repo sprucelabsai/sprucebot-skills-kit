20d65357e95a95971a6d32fa4896c304b466eded
Shane McDermed <shmcdermed@gmail.com>
2018-03-09 13:59:56 -0700

    Update tests snapshot with deps

--- interface/lang/default.js
+++ interface/lang/default.js
@@ -6,8 +6,9 @@ module.exports = {
 	teamDashboardBotText: ({ user }) =>
 		user.status === 'online'
 			? `You are at ${user.Location.name} as we speak! That's so cool! 🙌🏼`
-			: `Next time you get into ${user.Location
-					.name}, don't forget to join the wifi! 👊🏼`,
+			: `Next time you get into ${
+					user.Location.name
+			  }, don't forget to join the wifi! 👊🏼`,
 	teammateDashboardHeading: `Who's Online`,
 	errorLoadingGuests: `Oh no! I could not load guests!`,
 	errorLoadingTeammates: `Oh no! I could not load teammates!`,
